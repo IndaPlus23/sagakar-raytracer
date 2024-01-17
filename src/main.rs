3d27c6a376557729e7f32b5232a4dce43b63ced9
Saga Karlsson <01.karlssonsaga@gmail.com>
2024-01-17 17:07:46 +0100

    restructure, add diffuse and lambertian materials

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,86 @@
+use std::{io::Error, clone};
+use rand::{thread_rng, rngs::ThreadRng, Rng};
 use output::{write_bmp, write_tga};
 use glam::Vec3;
+use dyn_clone::DynClone;
 
-const OUTPUT_FORMAT: Format = Format::BMP;
+enum Face {
+    Front,
+    Back
+}
 
 enum Format {
     BMP,
     TGA
 }
 
+
+trait Material: DynClone {
+    // Get the bounced ray direction given an incoming ray and an outward normal
+    fn bounce_direction(&self, rng: &mut ThreadRng, incoming: &Ray, normal: Vec3) -> Vec3;
+    // Get the proportion of bounced blue, green and red light
+    fn brg_reflectivity(&self) -> (f32, f32, f32);
+}
+
 trait Object {
     // If ray intersects, return point of intersection
     // Else return none
-    fn intersect(&self, ray: &Ray) -> Option<Hit>;
+    fn intersect(&self, ray: &Ray, hit_interval: &Interval) -> Option<Hit>;
     // Return the unit normal at the given point
     fn normal(&self, point: Vec3) -> Vec3;
+    // Return the material of the object
+    fn get_material(&self) -> Box<dyn Material>;
 }
+
 
 // Contains information on a ray-object intersection
 struct Hit {
     t: f32,
     position: Vec3,
-    normal: Vec3
+    normal: Vec3,
+    face: Face,
+    material: Box<dyn Material>
 }
 
 impl Hit {
-    fn new(t: f32, position: Vec3, normal: Vec3) -> Hit {
+    fn new(ray: &Ray, t: f32, position: Vec3, outward_normal: Vec3, material: Box<dyn Material>) -> Hit {
+        let face = match outward_normal.dot(ray.direction) < 0.0 {
+            true => Face::Front,
+            false => Face::Back,
+        };
+        let normal = match face {
+            Face::Front => outward_normal,
+            Face::Back => -outward_normal
+        };
         Hit {
             t,
             position,
-            normal
+            normal,
+            face,
+            material
         }
+    }
+}
+
+struct Interval {
+    min: f32,
+    max: f32
+}
+
+impl Interval {
+    fn new(min: f32, max: f32) -> Interval {
+        Interval {
+            min,
+            max
+        }
+    }
+
+    fn contains(&self, num: f32) -> bool {
+        num >= self.min && num <= self.max 
+    }
+
+    fn surrounds(&self, num: f32) -> bool {
+        num > self.min && num < self.max
     }
 }
 
@@ -51,107 +102,248 @@ impl Ray {
     fn pos(&self, t: f32) -> Vec3 {
         self.origin + self.direction * t
     }
+
+    fn closest_intersection(&self, objects: &Vec<Box<dyn Object>>, hit_interval: &Interval) -> Option<Hit> {
+        let mut hit: Option<Hit> = None;
+        let mut closest = hit_interval.max;
+        for object in objects {
+            if let Some(this_hit) = object.intersect(self, &Interval::new(hit_interval.min, closest)) {
+                closest = this_hit.t;
+                hit = Some(this_hit);
+            }
+        }
+        return hit;
+    }
 }
 
-struct Sphere {
+struct Sphere<T: Material> {
     center: Vec3,
-    radius: f32
+    radius: f32,
+    material: Box<T>
 }
 
-impl Object for Sphere {
+impl<T: Material + 'static> Object for Sphere<T> {
     // Returns None if no hit, otherwise returns the t value at intersection
     // I used the pq formula for this, because the american formula is like math uncanny valley
-    fn intersect(&self, ray: &Ray) -> Option<Hit> {
+    // 
+    fn intersect(&self, ray: &Ray, hit_interval: &Interval) -> Option<Hit> {
         let center_to_origin = ray.origin - self.center;
         let half_p = ray.direction.dot(center_to_origin) / ray.direction.length_squared();
         let q = (center_to_origin.length_squared() - self.radius.powi(2)) / ray.direction.length_squared();
         let discriminant = half_p.powi(2) - q;
+        // If no real roots, return None
         if discriminant < 0.0 {
             return None;
         }
-        let t = -half_p - discriminant.sqrt();
+        let mut t = -half_p - discriminant.sqrt();
+        // If the negative root is not in the t interval, flip the root
+        if !hit_interval.surrounds(t) {
+            t = -half_p + discriminant.sqrt();
+            // If it's still not in the interval, it's too far away. Return None
+            if !hit_interval.surrounds(t) {
+                return None;
+            }
+        }
         let position = ray.pos(t);
-        return Some(Hit::new(t, position, self.normal(position)))
+        return Some(Hit::new(ray, t, position, self.normal(position), self.get_material()))
     }
 
     fn normal(&self, point: Vec3) -> Vec3 {
         (point - self.center).normalize()
     }
+
+    fn get_material(&self) -> Box<dyn Material> {
+        dyn_clone::clone_box(&*self.material)
+    }
 }
 
-impl Sphere {
-    fn new(center: Vec3, radius: f32) -> Sphere {
+impl<T: Material> Sphere<T>{
+    fn new(center: Vec3, radius: f32, material: T) -> Sphere<T> {
         Sphere {
             center,
-            radius
+            radius,
+            material: Box::new(material)
         }
     }
 }
 
-fn main() {
-    // Image dimenstsions
-    let image_width: u16 = 320;
-    let image_height: u16 = 240;
-    // Camera properties
-    let viewport_height: f32 = 2.0;
-    let viewport_width: f32 = viewport_height * (image_width as f32 / image_height as f32);
-    let focal_length: f32 = 1.0;
-    let camera_center= Vec3::new(0.0, 0.0, 0.0);
-    // Viewport coordinate system
-    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vec3::new(0.0, viewport_height, 0.0);
-    let pixel_delta_u = viewport_u / image_width as f32;
-    let pixel_delta_v = viewport_v / image_height as f32;
-    let viewport_lower_left = camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
-    let viewport_pixel_origin = viewport_lower_left + (pixel_delta_u + pixel_delta_v) / 2.0; // Pixel origin is inset by half a pixel width
-    
-    // Render
-    let objects: Vec<Box<dyn Object>> = vec![
-        Box::new(Sphere::new(Vec3::new(-0.5, 0.0, -1.0), 0.6)),
-        Box::new(Sphere::new(Vec3::new(0.7, 0.3, -1.2), 0.5))
-    ];
-
-    let mut image_data: Vec<Vec<u8>> = vec![]; // Color data, stored per row with the origin in the bottom left
-    image_data.resize(image_height as usize, vec![]);
-    // Scan left to right, bottom to top
-    for image_y in 0..image_height {
-        print!("\r{} lines remaining", image_height - image_y);
-        for image_x in 0..image_width {
-            let pixel_center = viewport_pixel_origin + image_x as f32 * pixel_delta_u + image_y as f32 * pixel_delta_v; 
-            let direction = pixel_center - camera_center;
-            let ray = Ray::new(camera_center, direction);
-            
-            let pixel_color = ray_to_color(&ray, &objects);
-            image_data[image_y as usize].extend_from_slice(&pixel_color);
-        }
-    }
-    
-    
-    write_bmp(&image_data, "output.bmp").unwrap();
-    write_tga(&image_data, "output.tga").unwrap();
+#[derive(Clone)]
+struct Diffuse {
+    color: Vec3,
 }
 
-fn ray_to_color(ray: &Ray, objects: &Vec<Box<dyn Object>>) -> [u8;3] {
-    for object in objects {
-        if let Some(hit) = object.intersect(ray) {
-            let normal = hit.normal;
-            let blue = lerp(55.0, 255.0, normal.x) as u8;
-            let green = lerp(55.0, 255.0, normal.y) as u8;
-            let red = lerp(55.0, 255.0, normal.z) as u8;
-            return [blue, green, red];
+impl Material for Diffuse {
+    fn bounce_direction(&self, rng: &mut ThreadRng, incoming: &Ray, normal: Vec3) -> Vec3 {
+        random_on_hemisphere(rng, &normal)
+    }
+
+    fn brg_reflectivity(&self) -> (f32, f32, f32) {
+        (self.color.x, self.color.y, self.color.z)
+    }
+}
+
+impl Diffuse {
+    fn new(color: Vec3) -> Diffuse {
+        Diffuse {color}
+    }
+}
+
+#[derive(Clone)]
+struct Lambertian {
+    color: Vec3
+}
+
+impl Material for Lambertian {
+    fn bounce_direction(&self, rng: &mut ThreadRng, incoming: &Ray, normal: Vec3) -> Vec3 {
+        normal + random_unit_vector(rng)
+    }
+
+    fn brg_reflectivity(&self) -> (f32, f32, f32) {
+        (self.color.x, self.color.y, self.color.z)
+    }
+}
+
+impl Lambertian {
+    fn new(color: Vec3) -> Lambertian {
+        Lambertian{color}
+    }
+}
+
+struct Camera {
+    image_width: u16,
+    image_height: u16,
+    center: Vec3,
+    pixel_delta_u: Vec3,
+    pixel_delta_v: Vec3,
+    viewport_pixel_origin: Vec3,
+    image_data: Vec<Vec<u8>>,
+    filename: String,
+    rng: ThreadRng,
+    samples: u32,
+    max_depth: u32,
+}
+
+impl Camera {
+    fn default() -> Camera {
+        let image_width: u16 = 320;
+        let image_height: u16 = 240;
+        let viewport_height: f32 = 2.0;
+        let viewport_width: f32 = viewport_height * (image_width as f32 / image_height as f32);
+        let focal_length: f32 = 1.0;
+        let center= Vec3::new(0.0, 0.0, 0.0);
+        let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+        let viewport_v = Vec3::new(0.0, viewport_height, 0.0);
+        let pixel_delta_u = viewport_u / image_width as f32;
+        let pixel_delta_v = viewport_v / image_height as f32;
+        let viewport_lower_left = center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+        let viewport_pixel_origin = viewport_lower_left + (pixel_delta_u + pixel_delta_v) / 2.0;
+        let mut image_data = vec![];
+        image_data.resize(image_height as usize, vec![]);
+        Camera {
+            image_width,
+            image_height,
+            center,
+            pixel_delta_u,
+            pixel_delta_v,
+            viewport_pixel_origin,
+            image_data,
+            filename: "output".to_owned(),
+            rng: thread_rng(),
+            samples: 100,
+            max_depth: 30,
         }
-    };
+    }
+
+    fn render(&mut self, objects: &Vec<Box<dyn Object>>, format: Format) -> Result<(), Error> {
+        // Scan left to right, bottom to top
+        for image_y in 0..self.image_height {
+            print!("\r{:3} lines remaining", self.image_height - image_y);
+            for image_x in 0..self.image_width {
+                // Sums to average the colors later
+                let (mut blue_sum, mut green_sum, mut red_sum) = (0u32, 0u32, 0u32);
+                for _i in 0..self.samples {
+                    let ray = self.get_random_ray(image_x, image_y);
+                    let (blue, green, red) = self.ray_to_color(&ray, &objects, self.max_depth);
+                    blue_sum += blue as u32;
+                    green_sum += green as u32;
+                    red_sum += red as u32;
+                }
+                // Average and add to image in LE order
+                self.image_data[image_y as usize].push((blue_sum / self.samples) as u8);
+                self.image_data[image_y as usize].push((green_sum / self.samples) as u8);
+                self.image_data[image_y as usize].push((red_sum / self.samples) as u8);
+
+            }
+        }
+        match format {
+            Format::BMP => write_bmp(&self.image_data, &(self.filename.clone() + ".bmp")),
+            Format::TGA => write_tga(&self.image_data, &(self.filename.clone() + ".tga"))
+        }
+    }
+
+    fn get_random_ray(&mut self, image_x: u16, image_y: u16) -> Ray {
+        let pixel_center = self.viewport_pixel_origin + image_x as f32 * self.pixel_delta_u + image_y as f32 * self.pixel_delta_v; 
+        let sample_offset = (-0.5 + self.rng.gen::<f32>()) * self.pixel_delta_u + (-0.5 + self.rng.gen::<f32>()) * self.pixel_delta_u;
+        let direction = pixel_center - self.center + sample_offset;
+        return Ray::new(self.center, direction);
+    }
+
+    fn ray_to_color(&mut self, ray: &Ray, objects: &Vec<Box<dyn Object>>, depth: u32) -> (u8, u8, u8) {
+        if depth == 0 {
+            return (0, 0, 0);
+        }
+        if let Some(hit) = ray.closest_intersection(objects, &Interval::new(0.001, f32::MAX)) {
+            let material = hit.material;
+            let direction = material.bounce_direction(&mut self.rng, ray, hit.normal);
+            let (reflect_blue, reflect_green, reflect_red) = material.brg_reflectivity();
+            let (bounced_blue, bounced_green, bounced_red) = self.ray_to_color(&Ray::new(hit.position, direction), objects, depth - 1);
+            let final_blue = (bounced_blue as f32 * reflect_blue) as u8;
+            let final_green = (bounced_green as f32 * reflect_green) as u8;
+            let final_red = (bounced_red as f32 * reflect_red) as u8;
+            return (final_blue, final_green, final_red)
+        }
+        return background_gradient(ray);
+    }
+}
+
+fn random_unit_vector(rng: &mut ThreadRng) -> Vec3 {
+    Vec3::new(rng.gen(), rng.gen(), rng.gen()).normalize()
+}
+
+fn random_on_hemisphere(rng: &mut ThreadRng, normal: &Vec3) -> Vec3 {
+    let vector = random_unit_vector(rng);
+    match vector.dot(*normal) > 0.0 {
+        true => vector,
+        false => -vector
+    }
+}
+
+fn background_gradient(ray: &Ray) -> (u8, u8, u8) {
     let direction = ray.direction.normalize();
     let t = direction.y;
     let blue = lerp(139.0, 255.0, t) as u8;
     let green = lerp(0.0, 255.0, t) as u8;
     let red = lerp(0.0, 255.0, t) as u8;
-    return [blue, green, red];
+    return (blue, green, red);
+}
+
+fn rand_in_interval(rng: &mut ThreadRng, interval: &Interval) -> f32 {
+    interval.min + (interval.max - interval.min) * rng.gen::<f32>()
 }
 
 // Linearly interpolates t ∈ [0, 1] to the range [v0, v1]
 fn lerp(v0: f32, v1: f32, t: f32) -> f32 {
     (1.0 - t) * v0 + t * v1
+}
+
+fn main() {
+    let mut camera = Camera::default();
+    // Render
+    let objects: Vec<Box<dyn Object>> = vec![
+        Box::new(Sphere::new(Vec3::new(-0.5, 0.0, -1.0), 0.6, Diffuse::new(Vec3::new(0.5, 0.5, 0.5)))),
+        Box::new(Sphere::new(Vec3::new(0.7, 0.3, -1.2), 0.5, Lambertian::new(Vec3::new(0.9, 0.4, 0.6)))),
+    ];
+    camera.render(&objects, Format::BMP).expect("Failed outputting image");
 }
 
 mod output {
